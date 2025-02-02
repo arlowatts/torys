6acba454f22bc9632b43ef93ddea026e02caeb03
Arlo Watts <arlowatts1@gmail.com>
2025-02-02 11:43:20 -0800

    Minor fix to spoiler script

--- scripts/spoiler.js
+++ scripts/spoiler.js
@@ -1,15 +1,21 @@
+// wait until the page loads
 addEventListener("load", main);
 
 function main() {
     let spoilerHidden = true;
+
+    // access the toggle switch to show and hide spoilers
     const spoilerToggle = document.getElementById("spoiler-toggle");
+
+    // access the style for the spoiler class
     const spoilerStyle = getStyleSheet("spoiler-style").cssRules[0].style;
 
+    // set up the event listener for the toggle switch
     spoilerToggle.addEventListener("click", () => {
         if (spoilerHidden) {
             spoilerHidden = false;
             spoilerToggle.textContent = "Hide spoilers";
-            spoilerStyle.display = "initial";
+            spoilerStyle.display = "";
         }
         else {
             spoilerHidden = true;
@@ -19,6 +25,7 @@ function main() {
     });
 }
 
+// access a stylesheet by the title attribute
 function getStyleSheet(title) {
     for (const sheet of document.styleSheets)
         if (sheet.title === title)
