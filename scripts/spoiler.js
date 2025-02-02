eb7f636af2f6974a0f68b151da3dbb88fd2daae0
Arlo Watts <arlowatts1@gmail.com>
2025-02-02 09:45:44 -0800

    Fix script to run after load

--- scripts/spoiler.js
+++ scripts/spoiler.js
@@ -1,19 +1,23 @@
-let spoilerHidden = true;
-const spoilerToggle = document.getElementById("spoiler-toggle");
-const spoilerStyle = getStyleSheet("spoiler-style").cssRules[0].style;
+addEventListener("load", main);
 
-spoilerToggle.addEventListener("click", () => {
-    if (spoilerHidden) {
-        spoilerHidden = false;
-        spoilerToggle.textContent = "Hide spoilers";
-        spoilerStyle.display = "initial";
-    }
-    else {
-        spoilerHidden = true;
-        spoilerToggle.textContent = "Show spoilers";
-        spoilerStyle.display = "none";
-    }
-});
+function main() {
+    let spoilerHidden = true;
+    const spoilerToggle = document.getElementById("spoiler-toggle");
+    const spoilerStyle = getStyleSheet("spoiler-style").cssRules[0].style;
+
+    spoilerToggle.addEventListener("click", () => {
+        if (spoilerHidden) {
+            spoilerHidden = false;
+            spoilerToggle.textContent = "Hide spoilers";
+            spoilerStyle.display = "initial";
+        }
+        else {
+            spoilerHidden = true;
+            spoilerToggle.textContent = "Show spoilers";
+            spoilerStyle.display = "none";
+        }
+    });
+}
 
 function getStyleSheet(title) {
     for (const sheet of document.styleSheets)
