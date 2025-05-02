d78339a60654c34b27d2fb84109a714666cfd741
Arlo Watts <arlowatts1@gmail.com>
2025-05-01 18:34:32 -0700

    Update automated scripts

---
+++ assets/scripts/page_links.py
@@ -0,0 +1,30 @@
+import os, re
+
+def main():
+    add_page_links("world")
+    add_page_links("stories")
+
+# replace the [[short links]] in each .md file with markdown links and liquid
+def add_page_links(path: str):
+
+    # if the path is a .md file, modify its content
+    if os.path.isfile(path) and os.path.splitext(path)[1] == ".md":
+
+        # open the file
+        with open(path) as file:
+            content = file.read()
+
+        # substitute the links
+        content = re.sub(r"\[\[(.+?)\]\]", r"[\1]({% assign page = site.pages | find: 'title', '\1' %}{{ page.url }})", content)
+
+        # write the changes to the file
+        with open(path, "w") as file:
+            file.write(content)
+
+    # if the path is a directory, recurse on its subpaths
+    elif os.path.isdir(path):
+        for relative_path in os.listdir(path):
+            add_spoiler_tags(os.path.join(path, relative_path))
+
+if __name__ == "__main__":
+    main()
--- assets/scripts/spoiler_tags.py
+++ assets/scripts/spoiler_tags.py
@@ -13,33 +13,47 @@ def add_spoiler_tags(path: str):
         # open the file
         with open(path) as file:
             content = ""
-
-            isSpoiler = False
+            is_spoiler = False
+            updated = False
 
             # update the spoiler flag or add an attribute to the line
             for line in file:
                 if line == "<spoiler>\n":
-                    isSpoiler = True
+                    if is_spoiler:
+                        raise SpoilerError(path)
+
+                    is_spoiler = True
+                    updated = True
 
                 elif line == "</spoiler>\n":
-                    isSpoiler = False
+                    if not is_spoiler:
+                        raise SpoilerError(path)
+
+                    is_spoiler = False
 
                 else:
-                    # add the spoiler tag to the previous block
-                    if isSpoiler and line.isspace():
+                    if is_spoiler and line.isspace():
                         content += "{: .spoiler}\n"
 
-                    # add the actual line to the file
                     content += line
 
+            if is_spoiler:
+                raise SpoilerError(path)
+
         # write the changes to the file
-        with open(path, "w") as file:
-            file.write(content)
+        if updated:
+            with open(path, "w") as file:
+                file.write(content)
 
     # if the path is a directory, recurse on its subpaths
     elif os.path.isdir(path):
-        for relativePath in os.listdir(path):
-            add_spoiler_tags(os.path.join(path, relativePath))
+        for relative_path in os.listdir(path):
+            add_spoiler_tags(os.path.join(path, relative_path))
+
+# use a custom exception class to report mismatched spoiler tags
+class SpoilerError(Exception):
+    def __init__(self, path: str):
+        super().__init__("mismatched spoiler tags in " + os.path.join(os.getcwd(), path))
 
 if __name__ == "__main__":
     main()
